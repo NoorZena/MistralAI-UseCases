a008478d45612d1c6db66c775369ec8d1afd04ee
Noor Zena <138724877+NoorZena@users.noreply.github.com>
2025-02-25 12:31:39 +0300

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from mistralai import Mistral, UserMessage
+import os
+
+# Load API Key
+api_key = "KbPCi7fSsq0GjlmJP6GejKFfmawcewnp"
+
+def mistral_response(user_message):
+    client = Mistral(api_key=api_key)
+    messages = [UserMessage(content=user_message)]
+    chat_response = client.chat.complete(model="mistral-large-latest", messages=messages)
+    return chat_response.choices[0].message.content
+
+# Streamlit UI
+st.title("Bank Customer Support Chatbot")
+user_input = st.text_input("Ask me a question:")
+
+if st.button("Submit"):
+    response = mistral_response(user_input)
+    st.write("### Response:", response)
